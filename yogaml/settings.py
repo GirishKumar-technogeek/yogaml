f68c54e47c97eab6dc056e2668077b58db44aaf2
Girish Kumar . S <56218745+GirishKumar-technogeek@users.noreply.github.com>
2022-03-26 10:06:42 +0530

    Update settings.py

--- yogaml/settings.py
+++ yogaml/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'yogaml.urls'
@@ -78,7 +76,7 @@ WSGI_APPLICATION = 'yogaml.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default="postgres://krvvnsopzystki:2877931aaedb363d22fa88280ef95fc91b4f3a7a370e1f1051074dc09fbd97bb@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d82bdp21l4rq74"
+        default="postgres://lorqoqsfxgslpc:0814d65a80f301d0cb6045a926619b1bb120c7c92277f7db9aebb3460c41c7f2@ec2-3-225-213-67.compute-1.amazonaws.com:5432/da3sfa5i6sotnl"
     )
 }
 
@@ -119,16 +117,21 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
+AWS_ACCESS_KEY_ID = "AKIAQB2ZOQQXQLKZ35P5"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = 'yogaml-assets'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+AWS_STATIC_LOCATION = 'static'
+STATICFILES_STORAGE = 'yogaml.storage_backends.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
 
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL = 'accounts/'
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
